04df22dc552f387ccc5d04fe6c698687b859579f
ts106370 <ts106370@login23-g-1.hpc.itc.rwth-aachen.de>
2026-05-05 13:55:29 +0200

    basic main py logic for data retrieval

--- ml_benchmark/main.py
+++ ml_benchmark/main.py
@@ -12,4 +12,19 @@ dali_extra_dir = os.environ['DALI_EXTRA_PATH']
 data_dir_2d = os.path.join(dali_extra_dir, 'db', '3D', 'MRI', 'Knee', 'npy_2d_slices', 'STU00001')
 data_dir_3d = os.path.join(dali_extra_dir, 'db', '3D', 'MRI', 'Knee', 'npy_3d', 'STU00001')
 
+data_dir = os.path.join(data_dir_2d, 'SER00001')
 
+@pipeline_def(batch_size=batch_size, num_threads=3, device_id=0)
+def pipe1():
+    data = fn.readers.numpy(device='cpu', file_root=data_dir, file_filter='*.npy')
+    return data
+
+def run(p):
+    p.build()  # build the pipeline
+    outputs = p.run()  # Run once
+    # Getting the batch as a list of numpy arrays, for displaying
+    batch = [np.array(outputs[0][s]) for s in range(batch_size)]
+    return batch
+
+data1 = run(pipe1())
+print(data1)
